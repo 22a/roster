8a6051e613ebeabce5ead763edb6479c6f19a0f3
Peter Meehan <peter.meehan@intercom.io>
2023-09-02 02:36:21 +0100

    disable React.StrictMode to prevent double renders in dev

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,10 @@ import { Helmet } from "react-helmet"
 import nightwind from "nightwind/helper"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <>
     <Helmet>
       <script>{nightwind.init()}</script>
     </Helmet>
     <App />
-  </React.StrictMode>,
+  </>,
 )
